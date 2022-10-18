39b79d686f5d42aa0723cf98d89bc02ef6b7dc1e
krylov <krylov@krista.ru>
2022-10-18 16:53:13 +0300

    Добавили log

--- src/App.tsx
+++ src/App.tsx
@@ -1,60 +1,61 @@
-import React, { FC, useEffect, useState } from "react"
-import "./App.css"
-import FilterBar from "./components/FilterBar"
-import ListOfPosts from "./components/Posts"
-import Comments from "./components/Comments"
-import EditPost from "./components/EditPost"
-import DeletePost from "./components/DeletePost"
+import React, { FC, useEffect, useState } from "react";
+import "./App.css";
+import FilterBar from "./components/FilterBar";
+import ListOfPosts from "./components/Posts";
+import Comments from "./components/Comments";
+import EditPost from "./components/EditPost";
+import DeletePost from "./components/DeletePost";
 
 interface PostItem {
-    userId: number
-    id: number
-    title: string
-    body: string
+    userId: number;
+    id: number;
+    title: string;
+    body: string;
 }
 function App() {
-    const [value, setValue] = useState("")
-    const [targetId, setTargetId] = useState(0)
-    const [showComments, setShowComments] = useState(false)
-    const [edit, setEdit] = useState(false)
-    const [posts, setPosts] = useState<PostItem[]>([])
-    const [fDelete, setFDelete] = useState(false)
-    const [doFilter, setDoFilter] = useState(false)
-    const [loadingPosts, setLoadingPosts] = useState(false)
+    const [value, setValue] = useState("");
+    const [targetId, setTargetId] = useState(0);
+    const [showComments, setShowComments] = useState(false);
+    const [edit, setEdit] = useState(false);
+    const [posts, setPosts] = useState<PostItem[]>([]);
+    const [fDelete, setFDelete] = useState(false);
+    const [doFilter, setDoFilter] = useState(false);
+    const [loadingPosts, setLoadingPosts] = useState(false);
 
     const deletePost = () => {
-        let newPosts = posts
-        delete newPosts[targetId - 1]
-        setPosts(newPosts)
-        setFDelete(false)
-    }
+        let newPosts = posts;
+        delete newPosts[targetId - 1];
+        setPosts(newPosts);
+        setFDelete(false);
+    };
 
     const saveEdit = (id: number, title: string, body: string) => {
-        posts[id - 1].title = title
-        posts[id - 1].body = body
-        setPosts(posts)
-        setEdit(false)
-    }
+        posts[id - 1].title = title;
+        posts[id - 1].body = body;
+        setPosts(posts);
+        setEdit(false);
+    };
 
     const getPostsData = async (url: string) => {
         try {
-            setLoadingPosts(true)
-            const response = await fetch(url)
-            const getPosts: any = await response.json()
-            setPosts(getPosts)
-            setLoadingPosts(false)
+            setLoadingPosts(true);
+            const response = await fetch(url);
+            const getPosts: any = await response.json();
+            setPosts(getPosts);
+            setLoadingPosts(false);
         } catch (e) {
-            console.log(e)
+            console.log(e);
         }
-    }
+    };
 
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setValue(e.target.value)
-    }
+        setValue(e.target.value);
+    };
 
     useEffect(() => {
-        getPostsData("https://jsonplaceholder.typicode.com/posts")
-    }, [])
+        console.log("123");
+        getPostsData("https://jsonplaceholder.typicode.com/posts");
+    }, []);
 
     return (
         <div className="app">
@@ -64,7 +65,7 @@ function App() {
                     handleChange(e)
                 }
                 change={() => {
-                    setDoFilter(!doFilter)
+                    setDoFilter(!doFilter);
                 }}
             />
             <ListOfPosts
@@ -72,16 +73,16 @@ function App() {
                 doFilter={doFilter}
                 isLoading={loadingPosts}
                 handleDelete={(id: number) => {
-                    setFDelete(true)
-                    setTargetId(id)
+                    setFDelete(true);
+                    setTargetId(id);
                 }}
                 handleEdit={(id: number) => {
-                    setEdit(true)
-                    setTargetId(id)
+                    setEdit(true);
+                    setTargetId(id);
                 }}
                 handleClick={(id) => {
-                    setShowComments(true)
-                    setTargetId(id)
+                    setShowComments(true);
+                    setTargetId(id);
                 }}
                 posts={posts}
             />
@@ -109,7 +110,7 @@ function App() {
                 />
             )}
         </div>
-    )
+    );
 }
 
-export default App
+export default App;
